02be49f260772c39ac63123a5c06c8e0fa48987f
DarcyOliveira <darcyelleoliveira@gmail.com>
2021-10-26 20:48:39 -0300

    Comitando estudos de blog pessoal

---
+++ MySQL Blog Pessoal/Blog Pessoal_mysql_create.sql
@@ -0,0 +1,36 @@
+create database db_blog_pessoal;
+use db_blog_pessoal;
+
+
+CREATE TABLE `Temas` (
+	`id` bigint NOT NULL,
+	`descricao` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Postagens` (
+	`id` bigint,
+	`titulo` varchar(255) NOT NULL,
+	`texto` varchar(255) NOT NULL,
+	`data` TIMESTAMP(255) NOT NULL,
+	`tema_id` bigint,
+	`usuario_id` bigint,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Usuarios` (
+	`id` bigint ,
+	`nome` varchar(255) NOT NULL,
+	`usuario` varchar(255) NOT NULL,
+	`senha` varchar(255) NOT NULL,
+	`senha` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `Postagens` ADD CONSTRAINT `Postagens_fk0` FOREIGN KEY (`tema_id`) REFERENCES `Temas`(`id`);
+
+ALTER TABLE `Postagens` ADD CONSTRAINT `Postagens_fk1` FOREIGN KEY (`usuario_id`) REFERENCES `Usuarios`(`id`);
+
+
+
+
