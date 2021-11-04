a416eb37075c4b2495e655f63b3c81fa6d822243
DarcyOliveira <darcyelleoliveira@gmail.com>
2021-11-03 21:12:57 -0300

    Atualizações de Tema em Blog Pessoal

--- MySQL Blog Pessoal/blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Postagem.java
+++ MySQL Blog Pessoal/blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Postagem.java
@@ -6,54 +6,76 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-@Entity  
-@Table(name = "tb_postagens")
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_postagem")
 
 public class Postagem {
 
-	@Id			
-	@GeneratedValue(strategy = GenerationType.IDENTITY)		
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
-	@NotNull(message = "O campo título é obrigatório!")	
-	@Size(min = 5, max = 100, message = "Min de caractéres 5 e máx 100")	
+
+	@NotNull(message = "O campo título é obrigatório!")
+	@Size(min = 5, max = 100, message = "Min de caractéres 5 e máx 100")
 	private String titulo;
-	
-	@NotNull(message = "O campo texto é obrigatório!")	
-	@Size(min = 10, max = 1000, message = "Min de caractéres 10 e máx 1000")	
+
+	@NotNull(message = "O campo texto é obrigatório!")
+	@Size(min = 10, max = 1000, message = "Min de caractéres 10 e máx 1000")
 	private String texto;
-	
+
 	@Temporal(TemporalType.TIMESTAMP)
-	private Date data = new java.sql.Date(System.currentTimeMillis()); 
-	
+	private Date data = new java.sql.Date(System.currentTimeMillis());
+
+	private Tema tema;
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getTexto() {
 		return texto;
 	}
+
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
+
 	public Date getData() {
 		return data;
 	}
+
 	public void setData(Date data) {
 		this.data = data;
+	}
+
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 }
